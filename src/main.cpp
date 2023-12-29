7dfa783cd51d9f95fb9252647e91845972d3997f
mennov <menno@vanderlist.ca>
2023-12-28 22:57:48 -0800

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,136 @@
-#include <Arduino.h>
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
 
-// put function declarations here:
-int myFunction(int, int);
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x64 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define PIN_SWITCH    4
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+void displayCenterText(String text) {
+  int16_t x1;
+  int16_t y1;
+  uint16_t width;
+  uint16_t height;
+
+  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
+
+  // display on horizontal and vertical center
+  display.clearDisplay(); // clear display
+  display.setCursor((SCREEN_WIDTH - width) / 2, (SCREEN_HEIGHT - height) / 2);
+  display.println(text); // text to display
+  display.display();
+}
+
+int _countdown = -10;
+int lastMillis = millis();
+
+void startCountdown() {
+  _countdown = 30;
+  lastMillis = millis();
+}
+
+bool countdown(int interval) {
+  if(millis() > (lastMillis + interval)) {
+    _countdown--;
+    lastMillis = millis();
+    return true;
+  }
+  return false;
+}
+
+void displayCountdown() {
+  Serial.printf("Count %d\n",countdown);
+  display.setTextSize(4); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  displayCenterText(String(_countdown));
+  display.display();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  pinMode(PIN_SWITCH, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(_countdown>-30 && countdown(200)) {
+    if(_countdown > 0) {
+      display.clearDisplay();
+      displayCountdown();
+      Serial.printf("Countdown at %d",_countdown);
+    } else if(_countdown > -10) {
+      displayCenterText("DONE");
+      Serial.printf("Done at %d",_countdown);
+    } else {
+      Serial.println("clear display");
+      display.clearDisplay();
+      display.display();
+    }
+  }
+  if(digitalRead(PIN_SWITCH)) {
+    // Serial.println("pin high");
+  } else {
+    startCountdown();
+    Serial.println("pin low");
+  }
 }
