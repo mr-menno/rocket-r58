1e3a7393bd6fd414f9faa5d6e47fbbdeb9d97ea1
mennov <menno@vanderlist.ca>
2023-12-29 22:42:48 -0800

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define NUMFLAKES     10 // Number of snowflakes in the animation example
 
 #define PIN_SWITCH    4
+#define PIN_MODE      16
+#define PIN_PUMP      17
 
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
@@ -74,13 +76,9 @@ void displayCenterText(String text) {
   display.display();
 }
 
-int _countdown = -10;
+bool pumpOn = false;
+int _countdown = 0;
 int lastMillis = millis();
-
-void startCountdown() {
-  _countdown = 30;
-  lastMillis = millis();
-}
 
 bool countdown(int interval) {
   if(millis() > (lastMillis + interval)) {
@@ -92,11 +90,154 @@ bool countdown(int interval) {
 }
 
 void displayCountdown() {
-  Serial.printf("Count %d\n",countdown);
+  display.clearDisplay();
   display.setTextSize(4); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
-  displayCenterText(String(_countdown));
+  if(_countdown>30) {
+    displayCenterText("PI: "+String(_countdown-30));
+  } else {
+    displayCenterText(String(_countdown));
+  }
   display.display();
+}
+
+
+int lastPinMode=millis();
+char mode='C';
+int lastDebug=millis();
+int autoReset=millis();
+
+void setModeManual() {
+  mode='M';
+  autoReset=millis();
+  Serial.println("switching to manual");
+  display.setTextSize(2); // Draw 2X-scale text
+  displayCenterText(F("MANUAL"));
+  display.display();
+}
+
+void setModeAuto() {
+  mode='C';
+  _countdown=-36;
+  Serial.println("switching to auto");
+  display.setTextSize(2); // Draw 2X-scale text
+  displayCenterText(F("AUTO"));
+  display.display();
+}
+
+void debugStats() {
+  if(lastDebug+1000<millis()) {
+    lastDebug=millis();
+    Serial.println("---");
+    Serial.print(F("Mode: "));
+    Serial.println(mode);
+    Serial.print(F("Pin S: "));
+    Serial.println(digitalRead(PIN_SWITCH));
+    Serial.print(F("Pin M: "));
+    Serial.println(digitalRead(PIN_MODE));
+    Serial.print(F("Pump State: "));
+    Serial.println(pumpOn);
+    if(mode=='M') {
+      if(digitalRead(PIN_SWITCH)) {
+        Serial.println("Pump Off");
+      } else {
+        Serial.println("Pump On");
+      }
+    } else if(mode=='C' && _countdown>-30) {
+      if(_countdown>0) {
+        Serial.print("Countdown: ");
+        Serial.println(_countdown);
+      } else {
+        Serial.print("Countdown: ");
+        Serial.println("DONE");
+      }
+    }
+  }
+}
+
+void runManual() {
+  pumpOn = !digitalRead(PIN_SWITCH);
+  display.clearDisplay();
+  display.setTextSize(2);
+  if(pumpOn) {
+    digitalWrite(PIN_PUMP,1);
+    displayCenterText("Manual On");
+  } else {
+    digitalWrite(PIN_PUMP,0);
+    displayCenterText("Manual Off");
+  }
+  display.display();
+}
+
+bool countdownActive = false;
+void runAuto() {
+  if(countdownActive && _countdown>0) {
+    if(countdown(1000)) {
+      if(_countdown>30) {
+        pumpOn=false;
+        display.setTextSize(4);
+        displayCenterText("PI: "+String(_countdown-30));
+        display.display();
+      } else {
+        pumpOn=true;
+        display.setTextSize(4);
+        displayCenterText(String(_countdown));
+        display.display();
+      }
+    }
+  } else if(countdownActive && countdown(1000)) {
+    pumpOn=false;
+    display.setTextSize(2);
+    displayCenterText("done");
+    display.display();
+  }
+  if(!countdownActive) {
+    pumpOn=false;
+    display.setTextSize(2);
+    displayCenterText("auto");
+    display.display();
+  }
+  if(digitalRead(PIN_SWITCH)) {
+    countdownActive=false;
+    pumpOn=false;
+  } else if(!digitalRead(PIN_SWITCH) && countdownActive==false) {
+    pumpOn=false;
+    _countdown=36;
+    countdownActive=true;
+  }
+  if(pumpOn) {
+    digitalWrite(PIN_PUMP,1);
+  } else {
+    digitalWrite(PIN_PUMP,0);
+  }
+}
+void runAuto2() {
+  if(!countdownActive && !digitalRead(PIN_SWITCH)) {
+    countdownActive=true;
+    _countdown=36;
+  }
+  if(_countdown>-30 && countdown(1000) && !digitalRead(PIN_SWITCH)) {
+    if(_countdown > 0 && _countdown<=30) {
+      // display.clearDisplay();
+      displayCountdown();
+    } else if(_countdown > -10) {
+      displayCenterText("DONE");
+      Serial.printf("Done at %d",_countdown);
+    } else {
+      Serial.println("clear display");
+      display.clearDisplay();
+      display.display();
+    }
+  }
+  if(_countdown<=-30) {
+    // countdownActive
+  }
+  //control pump
+  if(_countdown>0 && _countdown <= 30) {
+    digitalWrite(PIN_PUMP,1);
+  } else {
+    digitalWrite(PIN_PUMP,0);
+  }
 }
 
 void setup() {
@@ -108,29 +249,35 @@ void setup() {
     for(;;); // Don't proceed, loop forever
   }
 
+  delay(2000);
+  Serial.println("Setup: Clear Display()");
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  displayCenterText("Rocket R58");
+  display.display();
+  delay(1000);
   display.clearDisplay();
   pinMode(PIN_SWITCH, INPUT_PULLUP);
+  pinMode(PIN_MODE, INPUT_PULLUP);
+  pinMode(PIN_PUMP, OUTPUT);
+  setModeAuto();
 }
 
+
 void loop() {
-  if(_countdown>-30 && countdown(200)) {
-    if(_countdown > 0) {
-      display.clearDisplay();
-      displayCountdown();
-      Serial.printf("Countdown at %d",_countdown);
-    } else if(_countdown > -10) {
-      displayCenterText("DONE");
-      Serial.printf("Done at %d",_countdown);
-    } else {
-      Serial.println("clear display");
-      display.clearDisplay();
-      display.display();
-    }
+  debugStats();
+  if(mode=='C') {
+    runAuto();
+  } else if(mode=='M') {
+    runManual();
   }
-  if(digitalRead(PIN_SWITCH)) {
-    // Serial.println("pin high");
-  } else {
-    startCountdown();
-    Serial.println("pin low");
+  if(!digitalRead(PIN_MODE) && lastPinMode+500 < millis()) {
+    lastPinMode=millis();
+    if(mode=='C') {
+      setModeManual();
+    } else {
+      setModeAuto();
+    }
   }
 }
